b1d4b892a171652295b8f4bbf8a38750a13e7f26
Srbuhi <srbuhi.sarg@gmail.com>
2023-03-21 21:19:40 +0400

    Style meals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import classes from './AvailableMeals.module.css'
 
 const DUMMY_MEALS = [
     {
@@ -30,7 +31,7 @@ const DUMMY_MEALS = [
 const AvailableMeals = () => {
     const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>)
   return (
-    <section>
+    <section className={classes.meals}>
       <ul>{mealsList}</ul>
     </section>
   )
