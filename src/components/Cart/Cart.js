691a433cdd46699b707d41be98e8488d9f36504d
Srbuhi <srbuhi.sarg@gmail.com>
2023-03-22 23:56:37 +0400

    Create and style Cart

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import classes from './Cart.module.css'
+
+
+const Cart = () => {
+
+    const cartItem =<ul className={classes['atcart-item']}>
+                        {[
+                        {id: 'c1', name: 'sushi', amount: '2', price: '12.45'} // for example
+                        ].map((item) => {<li>item.name</li>})}
+                    </ul>
+
+  return (
+    <div>
+      {cartItem}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>35.84</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </div>
+  )
+}
+
+export default Cart
