53884f6330e5bd2329b5ccac7cf2cc4d890ff4a8
Srbuhi <srbuhi.sarg@gmail.com>
2023-03-23 01:32:50 +0400

    Use Modal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,7 +11,7 @@ const Cart = () => {
                     </ul>
 
   return (
-    <div>
+    <Modal>
       {cartItem}
       <div className={classes.total}>
         <span>Total Amount</span>
@@ -21,7 +21,7 @@ const Cart = () => {
         <button className={classes['button--alt']}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   )
 }
 
